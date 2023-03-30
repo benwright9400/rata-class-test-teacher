b815b2c5032bf00715917a41b6db0b361a2dab91
Ben Wright <benwright9400@gmail.com>
2023-03-30 18:10:32 +0100

    add teacher UI structure

--- src/components/Setup.js
+++ src/components/Setup.js
@@ -1,6 +1,7 @@
 import { School, Send } from "@mui/icons-material";
 import { Button, Card, CardContent, TextField, Slider, InputAdornment, IconButton, OutlinedInput } from "@mui/material";
 import React, { useState } from "react";
+import { Bar, BarChart, Pie, PieChart, ResponsiveContainer, XAxis } from "recharts";
 
 function Setup() {
     const [stage, setStage] = useState(0);
@@ -9,25 +10,7 @@ function Setup() {
 }
 
 function loadContent(stageNumber, stage, setStage) {
-    if(stageNumber === 0) {
-        return (<div>
-            <School style={{scale: '4', marginTop: '20%', marginBottom: '2rem'}} />
-            <h1 className="font-sans text-3xl font-semibold mb-2">Welcome to Rita</h1>
-            <button variant="outlined" color="success" className="text-gray-100 m-4 px-4 py-2 bg-red-800 rounded hover:bg-red-700" onClick={() => {setStage(stage + 1)}}>Continue</button>
-
-        </div>);
-    }
-    if(stageNumber === 1) {
-        return (<div style={{width: 'fit-content', margin: 'auto', marginTop: '20%'}}>
-
-                <h3 className="font-sans text-xl font-semibold mb-4">Enter your name</h3>
-                <TextField size="small" placeholder="Name" />
-                <br></br>
-                <button variant="outlined" color="success" className="text-gray-100 m-4 px-4 py-2 bg-red-800 rounded hover:bg-red-700" onClick={() => {setStage(stage + 1)}}>Continue</button>
-
-    </div>);
-    }
-    if(stageNumber === 2) {
+    let data = [{name: "10", value: 12}, {name: "20", value: 120}, {name: "30", value: 120}, {name: "40", value: 120}, {name: "40", value: 120}, {name: "50", value: 120},{name: "60", value: 120},{name: "70", value: 120},{name: "80", value: 120},{name: "90", value: 120},{name: "10", value: 120},];
         return (<div>
             <Card>
                 <div className="flex items-center justify-center flex-auto">
@@ -35,27 +18,24 @@ function loadContent(stageNumber, stage, setStage) {
                     <h2 className="font-semibold font-xl" style={{padding: '1rem', paddingLeft: '0.5rem'}}>Rita</h2>
                 </div>
             </Card>
-            <div className="p-4 absolute top-1/4 bottom-0 left-0 right-0">
-                <div className="grow-0">
+            <div className="p-4 flex">
+                <div className="w-1/2 mr-2">
                     <Card>
-                        <h3 className="font-semibold font-xl p-2 ">Confidence Slider</h3>
-                        <div className="p-2 px-4">
-                            <Slider
-                                style={{margin: '1rem;'}}
-                                defaultValue={30}
-                                valueLabelDisplay="auto"
-                                step={10}
-                                marks
-                                min={0}
-                                max={100}
-                                />
+                        <h3 className="font-semibold font-xl p-2 ">Confidence Breakdown</h3>
+                        <div className="p-2">
+                            <ResponsiveContainer width="100%" height={400}>
+                                <BarChart data={data}>
+                                    <Bar dataKey="value" nameKey="name" fill="#8884d8"/>
+                                    <XAxis dataKey="name" />
+                                </BarChart>
+                            </ResponsiveContainer>
                         </div>
                     </Card>
                 </div>
-                <div className="grow">
-                    <Card className="mt-4">
-                        <div className="p-2">
-                            <div className="h-64 text-left overflow-y-scroll my-2 px-2">
+                <div className="w-1/2 h-11/12">
+                    <Card className="h-11/12" style={{height: '80vh'}}>
+                        <div className="p-2 h-11/12">
+                            <div className="text-left overflow-y-scroll my-2 px-2" style={{height: '70vh'}}>
                                 <p>James: When is this due?</p>
                             </div>
                             <OutlinedInput size="small" style={{width: '100%'}}
@@ -70,8 +50,6 @@ function loadContent(stageNumber, stage, setStage) {
             </div>
             
         </div>);
-    }
-    return <div>End</div>;
 }
 
 export default Setup;
